245391c8c28123dfae1a1dd14279cb2bc09f26d8
apple <apple@apples-MacBook-Air.local>
2025-06-13 19:00:48 +0545

    add feat

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/fileanalyse", function (req, res) {
+  const multer = require("multer");
+  const upload = multer({ dest: "uploads/" });
 
+  upload.single("upfile")(req, res, function (err) {
+    if (err) {
+      return res.status(400).send({ error: "Error uploading file" });
+    }
 
+    if (!req.file) {
+      return res.status(400).send({ error: "No file uploaded" });
+    }
+
+    const fileInfo = {
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size,
+    };
+
+    res.json(fileInfo);
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
